eb04a697b68673a43b8c81c2684724d569f719ac
Shuydo <shuydo12@gmail.com>
2021-07-09 02:06:26 +0300

    add set/get LStorage

--- src/App.js
+++ src/App.js
@@ -8,19 +8,19 @@ import Filter from "./Comps/Filter/Filter";
 export default class App extends React.Component {
   state = {
     contacts: [
-      { id: "id-1", name: "Rousie Simpson", number: "459-12-56" },
-      { id: "id-2", name: "Hermeone Kline", number: "443-89-12" },
-      { id: "id-3", name: "Eaden Clements", number: "645-17-79" },
-      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+      // { id: "id-1", name: "Rousie Simpson", number: "459-12-56" },
+      // { id: "id-2", name: "Hermeone Kline", number: "443-89-12" },
+      // { id: "id-3", name: "Eaden Clements", number: "645-17-79" },
+      // { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
     ],
     filter: "",
   };
 
-  appendDBase = (data) => {
-    if (this.state.contacts.map((el) => el.name).includes(data.name))
+  appendDBase = data => {
+    if (this.state.contacts.map(el => el.name).includes(data.name))
       alert(`${data.name} is already in contacts.`);
     else
-      this.setState((pState) => ({
+      this.setState(pState => ({
         contacts: [
           ...pState.contacts,
           {
@@ -31,23 +31,35 @@ export default class App extends React.Component {
       }));
   };
 
-  changeFilter = (e) => {
+  changeFilter = e => {
     this.setState({ filter: e.target.value });
   };
 
   getFilterContacts = () => {
     const filterLowerC = this.state.filter.toLowerCase();
 
-    return this.state.contacts.filter((contact) =>
+    return this.state.contacts.filter(contact =>
       contact.name.toLowerCase().includes(filterLowerC)
     );
   };
 
-  deleteContact = (contactId) => {
-    this.setState((pState) => ({
-      contacts: pState.contacts.filter((contact) => contact.id !== contactId),
+  deleteContact = contactId => {
+    this.setState(pState => ({
+      contacts: pState.contacts.filter(contact => contact.id !== contactId),
     }));
   };
+
+  componentDidMount() {
+    // console.log("add componentDidMount");
+    const isContacts = JSON.parse(localStorage.getItem("contacts"));
+    if (isContacts) this.setState({ contacts: isContacts });
+  }
+
+  componentDidUpdate(pProps, pState) {
+    // console.log("add componentDidUpdate");
+    if (this.state.contacts !== pState.contacts)
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+  }
 
   render() {
     return (
